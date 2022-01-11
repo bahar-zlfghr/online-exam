2a7311406e3af2dd5b6b5eaa56688229a78b6919
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2022-01-11 10:44:15 +0330

    initial commit

---
+++ src/test/java/ir/ac/alzahra/onlineexam/OnlineExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ac.alzahra.onlineexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
